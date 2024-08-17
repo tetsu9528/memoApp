1938bfa4d6fd0243f8fd6067acac1baedf0a404a
tetsu9528 <ret.kiy1947@gmail.com>
2024-08-17 16:48:46 +0900

    全体の調整

--- js/createList.js
+++ js/createList.js
@@ -1,46 +1,61 @@
-"use-strict"
+"use-strict";
 {
-    const createInterestingList=()=>{
-        const fun=document.createElement("h3");
-        fun.textContent="面白かった箇所";
-    
-       
-        const reason=document.createElement("h3");
-        reason.textContent="理由";
-    
+ 
+  
+
+  const createInterestingList = () => {
+    const fun = document.createElement("h3");
+    fun.textContent = "面白かった箇所";
+
+    const reason = document.createElement("h3");
+    reason.textContent = "理由";
+
+    const interesting = document.getElementById("interesting");
+    const list = document.createElement("div");
+    list.classList.add("list");
+    list.appendChild(fun);
+    list.appendChild(createPointList());
+    list.appendChild(reason);
+    list.appendChild(createReasonList());
+    interesting.appendChild(list);
+  };
+
+  const createPointList = () => {
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+    li.contentEditable = "true";
+    li.id = "point";
+    ul.appendChild(li);
+    return ul;
+  };
+
+  const createReasonList = () => {
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+    li.contentEditable = "true";
+    li.id = "reason";
+    ul.appendChild(li);
+    return ul;
+  };
+
+  const createList = () => {
+    const list = createPointList();
+    document.getElementById("factor").appendChild(list);
+  };
+
+  document
+    .getElementById("createInterestingListButton")
+    .addEventListener("click", () => {
+      createInterestingList();
       
-       const interesting=document.getElementById("interesting");
-       const list=document.createElement("div");
-       list.classList.add("list");
-        list.appendChild(fun);
-        list.appendChild(createPointList());
-        list.appendChild(reason);
-        list.appendChild(createReasonList());
-       interesting.appendChild(list);
-    }
+    });
 
-    const createPointList=()=>{
-        const ul=document.createElement("ul");
-        const li=document.createElement("li");
-        li.contentEditable="true";
-        li.id="interestingPoint"
-        ul.appendChild(li);
-        return ul;
-    }
+  document.getElementById("createListButton").addEventListener("click", () => {
+    createList();
+  });
 
-    const createReasonList=()=>{
-        const ul=document.createElement("ul");
-        const li=document.createElement("li");
-        li.contentEditable="true";
-        li.id="interestingReason";
-        ul.appendChild(li);
-       return ul;
-        
-    }
 
-    
-    document.getElementById("createListButton").addEventListener("click",()=>{
-        createInterestingList();
-    })
+
+
 
 }
